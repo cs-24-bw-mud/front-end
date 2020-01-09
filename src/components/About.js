dd1613a785a577783eb5155369b6b45e92a83425
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-08 22:13:55 -0700

    Finished with about styling for now

--- src/components/About.js
+++ src/components/About.js
@@ -9,44 +9,53 @@ import tania from '../assets/tania.png';
 const About = () => {
     return(
         <div className="about-container">
-        <h2 className="about-title">Meet the Team</h2>
+        <h1>Meet the Team</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In metus vulputate eu scelerisque felis. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Amet nisl purus in mollis. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Tincidunt vitae semper quis lectus nulla at volutpat diam ut.  A arcu cursus vitae congue mauris rhoncus aenean. Vel fringilla est ullamcorper eget. Aliquet eget sit amet tellus cras adipiscing enim. Risus feugiat in ante metus dictum at. Amet mattis vulputate enim nulla aliquet.
         </p>
         <div className="team-container">
             {/* Alex */}
             <div className="team-member">
-                <img className="image" src={alex} alt=" Alex Gordon"></img>
-                <p className="name">Alex Gordon</p>
-                <a className="github" href="https://github.com/agordon1123" target="blank">Github</a>
+                <div className="img-container"> 
+                    <img src={alex} alt=" Alex Gordon"></img>
+                </div>
+                <h3 >Alex Gordon</h3>
+                <a href="https://github.com/agordon1123" target="blank">Github</a>
             </div>
             {/*Blake */}
             <div className="team-member">
-                <img className="image" src={blake} alt=" Blake Prouty"></img>
-                <p className="name">Blake Prouty</p>
-                <a className="github" href="https://github.com/Prouty89" target="blank">Github</a>
-                <a className="github" href="http://www.blakeprouty.me" target="blank">Portfolio</a>
+                <div className="img-container"> 
+                    <img src={blake} alt=" Blake Prouty"></img>
+                </div>
+                <h3 >Blake Prouty</h3>
+                <a href="https://github.com/Prouty89" target="blank">Github</a>
+                
             </div>
             {/*Jade */}
             <div className="team-member">
-                <img  className="image" src={jade} alt="Jade Lopez"></img>
-                <p className="name">Jade Lopez</p>
-                <a className="github" href="https://github.com/jadeli1720" target="blank">Github</a>
-                <a className="github" href="https://www.jadeelopez.com/" target="blank">Portfolio</a>
+                <div className="img-container"> 
+                    <img   src={jade} alt="Jade Lopez"></img>
+                </div>
+                <h3 >Jade Lopez</h3>
+                <a href="https://github.com/jadeli1720" target="blank">Github</a>
             </div>
             {/*Jose */}
             <div className="team-member">
-                <img className="image" src={jose} alt="Jose Restrepo"></img>
-                <p className="name">Jose Restrepo</p>
-                <a className="github" href="https://github.com/Artyk77" target="blank">GitHub</a>
+                <div className="img-container"> 
+                    <img src={jose} alt="Jose Restrepo"></img>
+                </div>
+                <h3 >Jose Restrepo</h3>
+                <a href="https://github.com/Artyk77" target="blank">GitHub</a>
             </div>
             {/*Tania */}
             <div className="team-member">
-                <img  className="image" src={tania} alt="Tania Keller"></img>
-                <p className="name">Tania Keller</p>
-                <a className="github" href="https://github.com/taniamichelle" target="blank">Github</a>
+                <div className="img-container"> 
+                    <img  src={tania} alt="Tania Keller"></img>
+                </div>
+                <h3 >Tania Keller</h3>
+                <a href="https://github.com/taniamichelle" target="blank">Github</a>
             </div>
         </div>
-        </div>
+    </div>
     )
 }
 
